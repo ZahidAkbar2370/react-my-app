5b359b2f8ccfc1bf49c817fbba9dd09c0d73e393
ZahidAkbar2370 <zahidjakhar2370@gmail.com>
2022-09-16 11:11:14 +0500

    login page with postman auth api

---
+++ src/components/loginform.js
@@ -0,0 +1,66 @@
+import React, {useState} from "react";
+import { ToastContainer, toast } from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css';
+import "./loginform.css"
+
+function LoginForm(){
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    
+    async function loginData(){
+        if(email == "" && password == ""){
+            toast("Email & Password Required!");
+           return false;
+       }
+        if(email == ""){
+             toast("Email Required!");
+            return false;
+        }
+        else if(password == ""){
+            toast("Password Required!");
+           return false;
+       }
+        var myHeaders = new Headers();
+        myHeaders.append("Accept", "application/json");
+
+        var formdata = new FormData();
+        formdata.append("email", email);
+        formdata.append("password", password);
+
+        var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: formdata,
+        redirect: 'follow'
+        };
+
+fetch("https://staging.hospyta.com/auth/login", requestOptions)
+  .then(response => response.text())
+  .then(result => {
+    const obj = JSON.parse(result)
+    if(obj.status == "success"){
+        toast("Login Successfully!");
+    }
+    else{
+        toast("Username or Password Invalid!");
+           return false;
+    }
+  })
+  .catch(error => console.log('error', error));
+        
+    }
+
+    return (
+        <div className="cover">
+            <h1>Login</h1>
+            <input id="inputField" type="text" placeholder="username" onChange={(e) => setEmail(e.target.value)} className="form-control" required="true" />
+            <input id="inputField"  type="password" placeholder="password"  onChange={(e) => setPassword(e.target.value)} className="form-control" required="true" />
+
+            <div className="login-btn" onClick={loginData}>Login</div>
+
+            <div><ToastContainer /></div>
+        </div>
+    )
+}
+
+export default LoginForm
---
+++ src1/App.js
@@ -0,0 +1,11 @@
+import LoginForm from "./containers/login";
+
+function App() {
+  return (
+    <div className="page">
+      <LoginForm />
+    </div>
+  );
+}
+
+export default App;
